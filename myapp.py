7f7e63c5bf13326d3bea4bda5296a324c8cf7374
Akira61 <otaku.fahad11@gmail.com>
2023-09-14 00:53:30 +0300

    insert to DB function done

--- myapp.py
+++ myapp.py
@@ -1,10 +1,28 @@
-from flask import flask, request
+# from flask import flask, request
+from flask import Flask
+import flask
 from flask_restful import Resource , Api
 from flask_cors import CORS
 import pickle
 import pandas as pd
+import mysql.connector
 
+#data base setup
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="m7y6s2q1l9",
+  database="hathiq"
+)
+mycursor = mydb.cursor()
 
+#function to insert data to DB
+def addDB(serial_number, status, image):
+    sql = "INSERT INTO products (serial_number, status, image) VALUES (%s, %s,%s)"
+    vals = (serial_number, status, image)
+    mycursor.execute(sql, vals)
+    mydb.commit()
+    print(mycursor.rowcount, 'record inserted.')
 
 app = flask(__name__)
 CORS(app)
